624815dd04c1bf6d675b6b5a3d510f61ace87702
Gulsharan Goraya <gulsharan@gmail.com>
2015-09-07 11:13:09 -0500

    Updated vendor versions

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,5 @@ description      'Installs/Configures a Play2 application'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends "java", "~> 1.14.0"
-depends "artifact", "~> 1.10.3"
+depends "java", "~> 1.35.0"
+depends "artifact", "~> 1.12.2"
